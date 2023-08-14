3bd5a85451bde957688a068869b9b836c390c923
RATPC-043 <bhaswati@redappletech.com>
2023-08-14 20:00:38 +0530

    add geo location

---
+++ HiScore/Screens/Location/DisableLocationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DisableLocationViewController.swift
+//  HiScore
+//
+//  Created by RATPC-043 on 14/08/23.
+//
+
+import Foundation
+class DisableLocationViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ HiScore/Screens/Location/GetLocationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GetLocationViewController.swift
+//  HiScore
+//
+//  Created by RATPC-043 on 14/08/23.
+//
+
+import Foundation
+class GetLocationViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
