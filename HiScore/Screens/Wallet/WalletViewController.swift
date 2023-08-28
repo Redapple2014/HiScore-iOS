d35f23912352ad790c217c29dcda2f792aa070fc
Sandeep Nag <sandeep@redappletech.com>
2023-08-28 15:54:52 +0530

    started  wallet

---
+++ HiScore/Screens/Wallet/WalletViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WalletViewController.swift
+//  HiScore
+//
+//  Created by PC-072 on 28/08/23.
+//
+
+import UIKit
+
+class WalletViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
